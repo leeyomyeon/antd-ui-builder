9fdafba3bc92e0d32c74d68eb5988711e3049c78
leeyomyeon <leeyomyeon@gmail.com>
2026-05-13 11:03:20 +0900

    update to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/antd-ui-builder/',  // GitHub 레포 이름으로
+  base: '/antd-ui-builder/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'antd-vendor': ['antd', '@ant-design/icons'],
+          'grid-vendor': ['react-grid-layout'],
+          'prism-vendor': ['prismjs'],
+        }
+      }
+    }
+  }
 })
